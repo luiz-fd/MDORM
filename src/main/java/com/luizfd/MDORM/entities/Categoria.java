9a8316a312338001b74a35b47ad592ca106202a9
Luiz F Dias <luizfd@gmail.com>
2023-05-17 14:12:01 -0300

    Relacionamento Atividade - Participante

--- src/main/java/com/luizfd/MDORM/entities/Categoria.java
+++ src/main/java/com/luizfd/MDORM/entities/Categoria.java
@@ -3,6 +3,7 @@ package com.luizfd.MDORM.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(columnDefinition = "TEXT")
 	private String descricao;
 	
 	@OneToMany(mappedBy = "categoria")
--- src/main/java/com/luizfd/MDORM/entities/Participante.java
+++ src/main/java/com/luizfd/MDORM/entities/Participante.java
@@ -1,9 +1,14 @@
 package com.luizfd.MDORM.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,7 +19,11 @@ public class Participante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	@Column(unique = true)
 	private String email;
+	
+	@ManyToMany(mappedBy = "participantes")
+	private Set<Atividade> atividades = new HashSet<>();
 	
 	public Participante() {
 		
@@ -49,6 +58,10 @@ public class Participante {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public Set<Atividade> getAtividades() {
+		return atividades;
 	}
 	
 
